1627044655ee7287036d2fbc9a40279375ef198f
OVVilas <ovargasvilas@yahoo.com.br>
2020-10-19 00:12:03 -0300

    Classe dominio carro

---
+++ Projeto_Git/src/domain/Carro.java
@@ -0,0 +1,97 @@
+package domain;
+
+public class Carro {
+    private int id;
+    private int placa;
+    private String modelo;
+    private String marca;
+    private int ano;
+    private String tipo;
+    private String cambio;
+    private String combustivel;
+    
+    public Carro(){}
+
+    public Carro(int id, int placa, String modelo, String marca, int ano, String tipo, String cambio, String combustivel) {
+        this.id = id;
+        this.placa = placa;
+        this.modelo = modelo;
+        this.marca = marca;
+        this.ano = ano;
+        this.tipo = tipo;
+        this.cambio = cambio;
+        this.combustivel = combustivel;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getPlaca() {
+        return placa;
+    }
+
+    public void setPlaca(int placa) {
+        this.placa = placa;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public int getAno() {
+        return ano;
+    }
+
+    public void setAno(int ano) {
+        this.ano = ano;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getCambio() {
+        return cambio;
+    }
+
+    public void setCambio(String cambio) {
+        this.cambio = cambio;
+    }
+
+    public String getCombustivel() {
+        return combustivel;
+    }
+
+    public void setCombustivel(String combustivel) {
+        this.combustivel = combustivel;
+    }
+
+    @Override
+    public String toString() {
+        return "Carro{" + "id=" + id + ", placa=" + placa + ", modelo=" + modelo + ", marca=" + marca + ", ano=" + ano + ", tipo=" + tipo + 
+                ", cambio=" + cambio + ", combustivel=" + combustivel + '}';
+    }
+    
+    
+}
